acf7ef38264aaed6eeed30a2cceb0a1bc0f436b4
ashleypawar <41638031+ashleypawar@users.noreply.github.com>
2020-05-22 12:46:38 -0400

    adding starting project

---
+++ BackgroundGenerator.js
@@ -0,0 +1,41 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1"); // the JS query selector also acts just like the CSS class selector, 
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let randomColor = document.getElementById("mybutton");
+
+function setGradient() {
+	body.style.background = 
+		"linear-gradient(to right, " 
+		+ color1.value 
+		+ ", " 
+		+ color2.value 
+		+ ")";
+	css.textContent = body.style.background + ";";
+	//textContent adds the textContent to the DOM 
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+
+
+
+/*color1.addEventListener("input", function (){
+	body.style.background = 
+		"linear-gradient(to right, " 
+		+ color1.value 
+		+ ", " 
+		+ color2.value 
+		+ ")";
+})
+
+color2.addEventListener("input", function() {
+	body.style.background = 
+		"linear-gradient(to right, " 
+		+ color1.value 
+		+ ", " 
+		+ color2.value 
+		+ ")";
+	console.log(color2.value)
+})*/
